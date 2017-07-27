c5d32bf58c4d3415b7b4890f6083136fe5d22fc9
Jai <sunjy0410@gmail.com>
2017-07-26 22:27:25 -0500

    initial word generator

--- src/jobs/wordGenerator.py
+++ src/jobs/wordGenerator.py
@@ -1,11 +1,25 @@
 import random
 
 class WordGenerator:
-    def __init__(self):
-        print("This is the coolest init ever")
-
-    alphabets = ['a', 'b', 'c' 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    vowels = [0, 4, 8, 14, 20, 24]
+    def __init__(self, firstLetter):
+        self.firstLetter = firstLetter
 
     def wordGenerator(self):
-        return self.alphabets[0] + self.alphabets[1]
+        alphabets = ['a', 'b', 'c' 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                     'u', 'v', 'w', 'x', 'y', 'z']
+
+        # To be used later to make sure the word is at least pronounceable
+        # Need to implement inserting vowels if it hasn't been chosen in certain iterations.
+        # vowels = [0, 4, 8, 14, 20, 24]
+
+        word = self.firstLetter
+        wordLength = random.randint(3,8)
+        while(len(word) < wordLength):
+            alphabet = alphabets[random.randrange(26)]
+            word += alphabet
+
+        return word
+
+# For lazy testing purposes right now
+testing = WordGenerator("a")
+print(testing.wordGenerator())
